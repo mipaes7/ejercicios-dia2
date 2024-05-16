16701b8028162bf0f6fa6f21d7b5b3919da64bc3
Miguel Pardal <miguelpardalesparis@gmail.com>
2024-05-16 09:37:00 +0200

    ejercicio2.js refactorizado

--- ejercicio2.js
+++ ejercicio2.js
@@ -1,13 +1,18 @@
+// function countWords(str) {
+//     let arr = str.split(' ');
+
+//     let count = 0;
+
+//     for (let i = 0; i < arr.length; i++) {
+//         count += 1;
+//     }
+
+//     return count;
+// }
+
 function countWords(str) {
     let arr = str.split(' ');
-
-    let count = 0;
-
-    for (let i = 0; i < arr.length; i++) {
-        count += 1;
-    }
-
-    return count;
+    return arr.length;
 }
 
 console.log(countWords('Hola Mundo diojgobodfjb dojfoij dojfoijd'));
